ae07fddcfcd975c8c12bf7534c2069df265975e7
pettengillbla <pettengillbla@urbandaleschools.com>
2024-09-17 12:58:31 -0500

    finished Jedi Training 2

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -4,22 +4,23 @@
 UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
 1.) How do you enter a single line comment in a program? Give an example. (1pt)
-you use the #
+you use the number sign to comment in a single line
+# Hello World!
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
 Then in the Python Console window, enter a=2 and b=5 and then all of the following and record the actual output.
 If the output is an error record the error and try to determine what the error is!
 Comment about any of your predictions that didn't match the actual output
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 = 7
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5                                   2.5
+b//a                = 2                                     2
+b**a                = 25                                    25
+b%a                 = 1                                     1
+a+B                 = 7                                     7
+type(42)            = 42                                    int     forgot what type did thought it would type out 42
+type(42.0)          = 42.0                                  float   forgot what type did thought it would type out 42.0
+type("C3PO")        = C3PO                                  str     forgot what type did thought it would type out C3PO
+type(True)          = True                                  bool    forgot what type did thought it would type out True
 
 
 3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
@@ -31,21 +32,23 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15                                          15.0    forgot that dividing turned an int into a float
+type(a)       = int                                         float   forgot that dividing turned an int into a float
 
 
 4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
 
 x,y = (4,5)
-a = 3(x + y)
+# did not have asterisk after the 3 and before the parenthesis
+a = 3*(x + y)
 a
 
 
 5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+# does not have parenthesis around x+y+z
+ave = (x+y+z)/3
 ave
 
 
